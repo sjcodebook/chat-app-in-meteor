05aca45d2c605a1cf4c8975fafe3f332eabe2a80
sj <jainsanmati846@gmail.com>
2019-10-16 14:25:08 +0530

    2

--- client/main.js
+++ client/main.js
@@ -5,18 +5,45 @@ Meteor.startup(() => {
 
   socket.on('connect', function() {
     console.log('Client connected');
+    let $messageForm = $('#messageForm');
+    let $message = $('#message');
+    let $chat = $('#chat');
+    let $userForm = $('#userForm');
+    let $userFormArea = $('#userFormArea');
+    let $messageArea = $('#messageArea');
+    let $users = $('#users');
+    let $username = $('#username');
+
+    $messageForm.submit(function(e) {
+      e.preventDefault();
+      socket.emit('send message', $message.val());
+      $message.val('');
+    });
+
+    socket.on('new message', function(data) {
+      $chat.append(
+        ' <div class="well"><strong>' + data.user + ': ' + data.msg + '</div>'
+      );
+    });
+
+    $userForm.submit(function(e) {
+      e.preventDefault();
+      socket.emit('new user', $username.val(), function(data) {
+        $userFormArea.hide();
+        $messageArea.show();
+      });
+      $username.val('');
+    });
+
+    socket.on('get users', function(data) {
+      let html = '';
+      for (let i = 0; i < data.length; i++) {
+        html += '<li class="list-group-item">' + data[i] + '</li>';
+      }
+      $users.html(html);
+    });
   });
   socket.on('disconnect', function() {
     console.log('Client disconnected');
-  });
-
-  // Get counter updates from server
-  let serverCounter = counter.get();
-  socket.on('counter', function(value) {
-    console.log(`counter changed on server: ${value}`);
-    if (serverCounter !== value) {
-      serverCounter = value;
-      counter.set(value);
-    }
   });
 });
--- server/main.js
+++ server/main.js
@@ -3,6 +3,8 @@ import http from 'http';
 import socket_io from 'socket.io';
 
 const PORT = parseInt(process.env.SOCKET_PORT) || 3003;
+const users = [],
+  connections = [];
 
 // Client-side config
 WebAppInternals.addStaticJs(`
@@ -16,8 +18,34 @@ Meteor.startup(() => {
 
   // New client
   io.on('connection', function(socket) {
-    console.log('new socket client');
+    connections.push(socket);
+    console.log('connected: %s sockets connected', connections.length);
+
+    // Disconnect
+    socket.on('disconnect', data => {
+      users.splice(users.indexOf(socket.username), 1);
+      updateUsernames();
+      connections.splice(connections.indexOf(data), 1);
+      console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
+    // Send Message
+    socket.on('send message', data => {
+      io.sockets.emit('new message', { msg: data, user: socket.username });
+    });
+
+    // New User
+    socket.on('new user', (data, callback) => {
+      callback(true);
+      socket.username = data;
+      users.push(socket.username);
+      updateUsernames();
+    });
   });
+
+  function updateUsernames() {
+    io.sockets.emit('get users', users);
+  }
 
   // Start server
   try {
